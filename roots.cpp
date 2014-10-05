4418a674dddbc742b9ee5f09bae0166eae5feac1
Drew Walton <drewwalton19216801@gmail.com>
2014-10-05 07:11:11 -0600

    roots: don't unmount anything, just make sure /cache (or /tmp) is available

--- roots.cpp
+++ roots.cpp
@@ -634,9 +634,6 @@ int setup_install_mounts() {
         if (strcmp(v->mount_point, "/tmp") == 0 ||
             strcmp(v->mount_point, "/cache") == 0) {
             if (ensure_path_mounted(v->mount_point) != 0) return -1;
-
-        } else {
-            if (ensure_path_unmounted(v->mount_point) != 0) return -1;
         }
     }
     return 0;
