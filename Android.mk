faaddd431da9dd2beac8bf62b77b64ac3518415d
Drew Walton <drewwalton19216801@gmail.com>
2014-10-24 20:53:26 -0600

    Fake change!
    Change-Id: I0cde61c6d8f064061e952151169969681b8fc205

--- Android.mk
+++ Android.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#FAKE CHANGE
+
 ifeq ($(call my-dir),$(call project-path-for,recovery))
 
 LOCAL_PATH := $(call my-dir)
