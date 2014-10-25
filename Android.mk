00e47056bf16c38941e1ffb68e1128323d8f504e
Drew Walton <drewwalton19216801@gmail.com>
2014-10-24 20:47:52 -0600

    Fake commit

--- Android.mk
+++ Android.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Fakery!
+
 ifeq ($(call my-dir),$(call project-path-for,recovery))
 
 LOCAL_PATH := $(call my-dir)
